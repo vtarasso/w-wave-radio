d4d4c60ec1233e114126bcd7b07cab30ba2122b7
Vitaliy Tarassov <apxustudio@gmail.com>
2023-10-16 18:03:40 +0600

    modal-login add)

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,27 +1,72 @@
-// Открыть модальное окно
-document.getElementById('modal-open').addEventListener('click', function() {
-  document.getElementById('my-modal').classList.add('open')
-})
-
-// Закрыть модальное окно
-document.getElementById('modal-close').addEventListener('click', function() {
-  document.getElementById('my-modal').classList.remove('open')
-})
-
-// Закрыть м.о. при помощи Esc
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    document.getElementById('my-modal').classList.remove('open')
+document.addEventListener('DOMContentLoaded', function () {
+  // Функция для открытия модального окна
+  function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.classList.add('open');
+    }
   }
-})
 
-// Закрыть м.о. при клике вне его
-document.querySelector('#my-modal .modal__box').addEventListener('click', event => {
-  event._isClickWithInModal = true
-})
+  // Функция для закрытия модального окна
+  function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.classList.remove('open');
+    }
+  }
 
-document.getElementById('my-modal').addEventListener('click', event => {
-  if (event._isClickWithInModal) return
-  event.currentTarget.classList.remove('open')
-})
+  // Открытие модального окна входа
+  document.getElementById('modal-open').addEventListener('click', () => {
+    openModal('signIn-modal');
+  });
 
+  // Закрытие модального окна входа
+  document.getElementById('signIn-modal-close').addEventListener('click', () => {
+    closeModal('signIn-modal');
+  });
+
+  // Закрытие модального окна при нажатии клавиши "Escape"
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      closeModal('signIn-modal');
+      closeModal('login-modal');
+    }
+  });
+
+  // Открытие модального окна регистрации
+  document.getElementById('login-modal-btn').addEventListener('click', () => {
+    closeModal('signIn-modal');
+    openModal('login-modal');
+  });
+
+  // Открытие модального окна входа из регистрации
+  document.getElementById('signIn-modal-log').addEventListener('click', () => {
+    closeModal('login-modal');
+    openModal('signIn-modal');
+  });
+
+  // Закрытие модального окна регистрации
+  document.getElementById('login-modal-close').addEventListener('click', () => {
+    closeModal('login-modal');
+  });
+
+  // Закрыть м.о. при клике вне его - входа
+  document.querySelector('#signIn-modal .modal__box').addEventListener('click', event => {
+    event._isClickWithInModal = true
+  })
+
+  document.getElementById('signIn-modal').addEventListener('click', event => {
+    if (event._isClickWithInModal) return
+    event.currentTarget.classList.remove('open')
+  })
+
+  // Закрыть м.о. при клике вне его - регистрации
+  document.querySelector('#login-modal .modal__box').addEventListener('click', event => {
+    event._isClickWithInModal = true
+  })
+
+  document.getElementById('login-modal').addEventListener('click', event => {
+    if (event._isClickWithInModal) return
+    event.currentTarget.classList.remove('open')
+  })
+});
